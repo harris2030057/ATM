302c0b37cbadfdd3c1763e784cd651c8e4f69c15
Harris <harris2030057@gmail.com>
2017-11-28 11:48:02 +0800

    final

--- app/src/main/java/com/harris/atm/MainActivity.java
+++ app/src/main/java/com/harris/atm/MainActivity.java
@@ -20,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(!logon){
-            Intent
+            Intent intent = new Intent(this , LoginActivity.class);
+            startActivity(intent);
+
+
         }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
