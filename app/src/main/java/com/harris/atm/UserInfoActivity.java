74794ef83a75b14352d2f146e0aca214d7d80b8c
harris2030057 <harris2030057@gmail.com>
2017-12-19 01:16:57 +0800

    homework

--- app/src/main/java/com/harris/atm/UserInfoActivity.java
+++ app/src/main/java/com/harris/atm/UserInfoActivity.java
@@ -8,17 +8,25 @@ import android.widget.EditText;
 
 public class UserInfoActivity extends AppCompatActivity {
     private final static  int FUNC_BACK = 102;
+    private EditText edphone;
+    private EditText edname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        edname = (EditText) findViewById(R.id.edname);
+        edphone = (EditText) findViewById(R.id.edphone);
+        String edName = getSharedPreferences("info",MODE_PRIVATE).getString("Name","");
+        String edPhone = getSharedPreferences("info",MODE_PRIVATE).getString("Phone","");
+        edname.setText(edName);
+        edphone.setText(edPhone);
+
     }
     public  void back(View view){
 //       Intent intent = new Intent(this , MainActivity.class);
 //           /* startActivity(intent);*/
 //        startActivityForResult(intent , FUNC_BACK);
-        EditText edname = (EditText) findViewById(R.id.edname);
-        EditText edphone = (EditText) findViewById(R.id.edphone);
         String name = edname.getText().toString();
         String phone = edname.getText().toString();
         getIntent().putExtra("Nick name", name);
